e24d2262acc16b3b94985855b8c5a4f345ef6dfa
dania00610 <danochms2@gmail.com>
2025-04-07 15:36:05 +0200

    Initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,41 @@
+
+export default function Navbar() {
+
+  return (
+    <nav
+      className="flex items-center justify-between px-6 py-4 shadow"
+      style={{ backgroundColor: "#3251D0" }}
+    >
+      <h1 className="text-white text-2xl font-bold">User Management</h1>
+      <div className="flex items-center gap-4   rounded-lg hover:bg-blue-700 transition-all">
+  
+  <button className="bg-white text-[#3251D0] px-4 py-2 rounded hover:bg-gray-100 font-semibold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+    Create User
+  </button>
+
+  <button className="bg-red-600 text-white px-4 py-2 rounded hover:bg-black font-semibold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+    Logout
+  </button>
+  <button className="text-white hover:text-gray-200 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fas"
+      data-icon="moon"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 384 512"
+      className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 lg:w-8 lg:h-8 xl:w-10 xl:h-10"
+    >
+      <path
+        fill="currentColor"
+        d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"
+      />
+    </svg>
+  </button>
+</div>
+
+
+    </nav>
+  );
+}
---
+++ src/components/usercard.tsx
@@ -0,0 +1,135 @@
+"react";
+
+
+interface User {
+  initial: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  status: string;
+  dob: string;
+}
+
+const UserCard = ({ user }: { user: User }) => {
+  return (
+    <div className="m-[2px] border-2 border-solid border-gray-50 shadow-md rounded-[8px] p-4">
+      <div className="flex flex-col items-center justify-between">
+        {/* User Initial in Circle */}
+        <div className="w-16 h-16 rounded-full bg-[#3251D0] text-white flex items-center justify-center text-2xl font-semibold">
+          {user.initial}
+        </div>
+
+        {/* User Info */}
+        <div className="text-left mt-4 space-y-1 w-full">
+          <div className="text-lg font-semibold">{`${user.firstName} ${user.lastName}`}</div>
+          <div className="text-sm text-gray-500">{user.email}</div>
+          <div className="text-sm text-gray-500">Status: {user.status}</div>
+          <div className="text-sm text-gray-500">DOB: {user.dob}</div>
+        </div>
+
+        {/* Buttons */}
+        <div className="mt-4 flex justify-end space-x-2 w-full">
+          <button className="bg-[#3251D0] text-white  py-1 px-3 rounded-lg text-sm hover:bg-red-500 transition-all">
+            Edit
+          </button>
+          <button className="bg-red-600 text-white py-1 px-3 rounded-lg text-sm hover:bg-red-500 transition-all">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const UserCardGrid = () => {
+  const users: User[] = [
+    {
+      initial: 'JD',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+      status: 'Active',
+      dob: '1990-05-15',
+    },
+    {
+        initial: 'JS',
+        firstName: 'Jane',
+        lastName: 'Smith',
+        email: 'jane.smith@example.com',
+        status: 'locked',
+        dob: '1988-10-22',
+      },
+      {
+        initial: 'AJ',
+        firstName: 'Alice',
+        lastName: 'Johnson',
+        email: 'alice.johnson@example.com',
+        status: 'Active',
+        dob: '1995-02-10',
+      },
+      {
+        initial: 'b',
+        firstName: 'Bob',
+        lastName: '',
+        email: 'bob.martin@example.com',
+        status: 'locked',
+        dob: '1980-08-05',
+      },
+      {
+        initial: 'CB',
+        firstName: 'Charlie',
+        lastName: 'Brown',
+        email: 'charlie.brown@example.com',
+        status: 'Active',
+        dob: '1992-11-30',
+      },
+      {
+        initial: 'DL',
+        firstName: 'David',
+        lastName: 'Lee',
+        email: 'david.lee@example.com',
+        status: 'locked',
+        dob: '1987-07-14',
+      }, {
+        initial: 'E',
+        firstName: 'Eve',
+        lastName: '',
+        email: 'eve.green@example.com',
+        status: 'Active',
+        dob: '1990-12-15',
+      }, {
+        initial: 'FW',
+        firstName: 'Frank',
+        lastName: 'white',
+        email: 'frank.white@example.com',
+        status: 'Active',
+        dob: '1994-01-25',
+      }, {
+        initial: 'GB',
+        firstName: 'Grace',
+        lastName: 'Black',
+        email: 'grace.black@example.com',
+        status: 'locked',
+        dob: '1985-03-17',
+      }, {
+        initial: 'H',
+        firstName: 'Hannah',
+        lastName: '',
+        email: 'hannah.purple@example.com',
+        status: 'Active',
+        dob: '1996-12-03',
+      },
+  ];
+
+  return (
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    {users.map((user, index) => (
+      <UserCard key={index} user={user} />
+    ))}
+  
+  
+    </div>
+  );
+};
+
+export default UserCardGrid;
